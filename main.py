b4ec1a51c7f9ce1dbe6e08adcf50a32a93f503cb
Terrence Plunkett <eightys3v3n+github@gmail.com>
2017-01-24 20:06:26 -0700

    WOO

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ if __name__ == "__main__":
       # create a shape that will draw lines around the simulation boundries
       bounds = ScreenObject("rectangle",[Position3f(0,0,-4),Position3f(10,10,0)])
 
-      sphere = ScreenObject("sphere",[Position3f(0,0,0),Position3f(4,4,4)])
+      sphere = ScreenObject("sphere",[Position3f(0,0,0),Position3f(1,100,100)])
       window.load_sphere(sphere)
 
-      #sphere1 = ScreenObject("sphere",[Position3f(0,0,0),Position3f(10,20,10)])
-      #window.load_sphere(sphere1)
+      sphere1 = ScreenObject("sphere",[Position3f(2,2,0),Position3f(1,100,100)])
+      window.load_sphere(sphere1)
 
-      #sphere2 = ScreenObject("sphere",[Position3f(0,0,0),Position3f(10,20,10)])
-      #window.load_sphere(sphere2)
+      sphere2 = ScreenObject("sphere",[Position3f(2,0,2),Position3f(1,100,100)])
+      window.load_sphere(sphere2)
 
       # load the bounds shape into the window so it is being drawn
       window.load_object(bounds)
