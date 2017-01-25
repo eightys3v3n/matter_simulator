5b440f9d25c2b581df07ec5e684b316c5d086180
Terrence Plunkett <eightys3v3n+github@gmail.com>
2017-01-24 19:22:06 -0700

    changed point to vector :P

--- Vector thang.py
+++ Vector thang.py
@@ -1,26 +1,26 @@
 n=5
 
-point1 = [2,5,7]
-point2 = [8,10,3]
-point3 = [7,12,13]
-point4 = [1,2,3]
-point5 = [7,2,4]
+vector1 = [2,5,7]
+vector2 = [8,10,3]
+vector3 = [7,12,13]
+vector4 = [1,2,3]
+vector5 = [7,2,4]
 
 m=0
 k=0
 
-points=[point1,point2,point3,point4,point5]
-x=points[k]
-y=points[m]
-for x in points:
-	for y in points:
+vectors=[vector1,vector2,vector3,vector4,vector5]
+x=vectors[k]
+y=vectors[m]
+for x in vectors:
+	for y in vectors:
 		if x is y:
 			m =+ 1
 			if m == n:
-				m=0 
+				m=0
 				k =+ 1
 				break
-		else: 
+		else:
 			r = []
 			ri = y[0] - x[0]
 			r.append(ri)
@@ -29,9 +29,8 @@ for x in points:
 			rk = y[2] - x[2]
 			r.append(rk)
 			print(r)
-			m =+ 1 
+			m =+ 1
 			if m == n:
 				m=0
 				k =+ 1
 				break
-	
