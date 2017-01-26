c9d92fca3f49267ee03aec8ea6cee31e5e76a03d
cuddlefishez <habermillersean@gmail.com>
2017-01-26 16:40:20 -0700

    extra stuff

--- acceleration.py
+++ acceleration.py
@@ -1,22 +1,25 @@
-# creates a field for every particle
-# mass is the mass of the particle emiting the field
-# s is the speparation vector between the particle that is being observed
-# essentially this will give acceleration of every particle
-# G is the gravitational constant 6.67408 * (10**-11)
-#sum the fields of every particle on 1 particle to determine acceleration
-#then do this for every particle
+
 from particle import Particle 
 from space import Postion3f, Vector3f
 import variables 
-from Vector thang import interactions
-r = interactions(vectors, x, y) 
-   n = 0
-for 5 > n:
-   position = Vector3f(r[n])
-   n =+ 1
-#s is just the vector and s_m can be replaced with the dot product
-#mass can be related to the particular particle
-#this function returns acceleration acting on particle B from A,it does not return force, to obtain force simply add all accelerations on B and multiply by Mass of B
+from Displace import interactions
+v1 = Vector3f(self.position)
+v2 = Vector3f(self.position)
+v3 = Vector3f(self.position)
+v4 = Vector3f(self.position)
+v5 = Vector3f(self.position)
+
+m=0
+k=0
+
+vectors=[]
+vectors.append(v1)
+vectors.append(v2)
+vectors.append(v3)
+vectors.append(v4)
+vectors.append(v5)
+
+# okay so what needs to happen is that position which is the vector towards the parent particle needs to have that particles mass associated with it, and then gravity will work
 def gravitaitonal_Fields(self.mass, G, self.acceleration, position):
   scalar = G*self.mass / dot.position(position)
   unit_vector = Vector3f(Postion3f(1,1,1))
@@ -26,15 +29,3 @@ def gravitaitonal_Fields(self.mass, G, self.acceleration, position):
 
 
 
-#mass = randint(1,700000000)
-#s = [randint(0,50000),randint(0,50000),randint(0,50000)]
-#s_m = (s[0]**2 +s[1]**2+s[2]**2)
-#a = gravitaitonal_Fields(mass,s,s_m)
-#print (a)
-
-#input ("press enter to end")
-
-
-#for every frame:
-	#for all masses compared to all other masses:
-		#gravitaitonal_Fields(mass,s)
