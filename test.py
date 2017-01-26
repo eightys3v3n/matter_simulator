3de7abad428e117070d157a8fdf564c1b9f70f5e
Terrence Plunkett <eightys3v3n+github@gmail.com>
2017-01-26 16:39:43 -0700

    added help text for the test framework

--- test.py
+++ test.py
@@ -4,12 +4,15 @@ from types import ModuleType
 import test_files
 
 
-# Test File
-# this file has the classes that make testing nice and easy ;)
-# this stuff is used in the tests.py file.
+"""
+Contains a framework for automatically compiling and running tests
+"""
 
 
 class Test():
+  """
+  Stores a refernece to the actual test function, and a list of tests that must be run before this one is
+  """
   def __init__(self,function,requires):
     self.function = function
     self.requires = requires
@@ -17,14 +20,20 @@ class Test():
 
 
   def __getattr__(self,key):
+    """
+    .name:            return the name of the test function with a ' test' added on
+    .function_name:   returns the name of the test function
+    """
     if key == "name":
       return self.function.__name__+" test"
     elif key == "function_name":
       return self.function.__name__
 
 
-
   def __call__(self):
+    """
+    Runs the function referenced if it has not already been completed
+    """
     if self.completed:
       return False
 
@@ -35,12 +44,22 @@ class Test():
 
 
 class Tests():
+  """
+  A collection of Test objects with no duplicates.
+  This class handles running all of the tests in the correct order
+  """
   def __init__(self):
     self.completed = []
     self.tests = []
 
 
   def already_testing(self,function):
+    """
+    param function: a reference to a function
+    returns:
+      True: if the test function is already in this collection of tests; i.e. it doesn't need to be added again
+      False: if the test function is not in this collection of tests
+    """
     for test in self.tests:
       if test.function == function:
         return True
@@ -48,11 +67,22 @@ class Tests():
 
 
   def new(self,function,func_requires=[]):
+    """
+    Adds a new test and it's prerequisites to the collection
+
+    param function: a reference to the test function
+    param func_requires: an array of references to the functions that must be run before this one
+    """
     if not self.already_testing(function):
       self.tests.append(Test(function,func_requires))
 
 
   def run_test(self,test):
+    """
+    Runs the Test object after all of it's prerequisites
+
+    param test: A Test object
+    """
     if not test.completed:
       for req in test.requires:
         req_test = None
@@ -87,6 +117,9 @@ class Tests():
 
 
   def __call__(self):
+    """
+    Runs all the tests in the collection
+    """
     for test in self.tests:
       self.run_test(test)
 
@@ -95,6 +128,12 @@ tests = Tests()
 
 
 def compile_tests(module):
+  """
+  Adds all the functions that meet a criteria to the global Tests object
+  criteria:
+    function ends in '_Test'
+    an array called 'functions name'+'_req' that is [] or contains references to other test functions
+  """
   for function_str in dir(module):
     function = getattr(module,function_str)
 
@@ -108,5 +147,9 @@ def compile_tests(module):
 
 
 def test_all():
+  """
+  Compiles and runs all the tests included in the specified module
+  Currently retrieving all tests from 'test_files.py'
+  """
   compile_tests(test_files)
   tests()
