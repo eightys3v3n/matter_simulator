c05bf218ae74152d183b3c5fb6adf650603a0c9b
Terrence Plunkett <eightys3v3n+github@gmail.com>
2017-01-26 16:11:41 -0700

    accidentally missed #

--- simulation.py
+++ simulation.py
@@ -15,14 +15,14 @@ import variables,space,acceleration
 
 class Simulation:
   def __init__(self,screen_objects):
-  
+
     # an array of the particles
     self.particles = []
-    
+
     # an array of the ScreenObjects for all the particles
     self.screen_objects = screen_objects
-    
-    
+
+
     self.new_particle()
     self.new_particle()
     self.new_particle()
@@ -48,19 +48,19 @@ class Simulation:
   # create a new particle and start drawing it
   def new_particle(self):
     particle = Particle()
-    
+
     # random position between -5,-5,-5 and 5,5,5
     particle.position = Random3f([-5,5],[-5,5],[-5,5])
-    
+
     # random velocity between -0.01,-0.01,-0.01 and 0.01,0.01,0.01
     particle.velocity = space.Vector3f(Random3f([-0.01,0.01],[-0.01,0.01],[-0.01,0.01]))
 
     # save the new particle
     self.particles.append(particle)
 
-    create a ScreenObject for drawing the new particle
+    # create a ScreenObject for drawing the new particle
     screen_object = ScreenObject("sphere",self.particles[len(self.particles)-1].
       position,radius=self.particles[len(self.particles)-1].radius)
-      
+
     # save the screen object
     self.screen_objects.append(screen_object)
